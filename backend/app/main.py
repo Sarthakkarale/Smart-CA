1e7642aae0ecf52b375c9d9f238de19cfe21ba9b
Siddhi Raskar <siddhiraskar395@gmail.com>
2026-06-27 21:59:16 +0530

    configuration is completed

---
+++ backend/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION
+)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Smart CA Authentication Backend Running"
+    }
